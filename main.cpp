8b653c966762b83a69b0abd26ae9754fae42ac2d
brandon <brandon_charest@yahoo.com>
2018-05-20 20:39:33 -0400

    Added MainGame

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include <Windows.h>
-#include <SDL/SDL.h>
-#include <GL/glew.h>
-
-
+#include "MainGame.h"
+#include <iostream>
 int main(int argc, char **argv)
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	MainGame mainGame;
+
+	mainGame.run();	
+
+	std::cin.get();
 
 	return 0;
 }
